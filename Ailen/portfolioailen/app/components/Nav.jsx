85d27e11daf33c74a34cbe6d039339b3230439b0
Beltru <belvader007@gmail.com>
2025-10-31 12:01:37 -0300

    empezado el portfolio ailu

---
+++ Ailen/portfolioailen/app/components/Nav.jsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full fixed top-2 left-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        {/* Logo */}
+        <p className="text-xl font-semibold text-black">
+          Ailén de Barbará
+        </p>
+
+        {/* Links */}
+        <div className="space-x-12">
+            <Link href="/contact" className="text-black border-black border px-4 py-2 rounded-xl">
+            Contact
+          </Link>
+          <Link href="/" className="text-black">
+            About me
+          </Link>
+          <Link href="/about" className="text-black">
+            Work
+          </Link>
+          <Link href="/services" className="text-black">
+            Projects
+          </Link>
+        
+        </div>
+      </div>
+    </nav>
+  );
+}
--- Ailen/portfolioailen/app/page.jsx
+++ Ailen/portfolioailen/app/page.jsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
+import Navbar from "./components/Nav";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Ailén</h1>
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <Navbar></Navbar>
+      <main className="flex min-h-screen w-full flex-col items-center justify-center">
+        <section className="flex flex-row w-full h-screen items-center justify-center ">
+          <img src="imagen.png" className="h-[50vh]"/>
+          <div className="flex flex-col">
+            <l1 className="text-black text-8xl">TFOLIO</l1>
+            <ul>
+              <div className="bg-gray-100 h-[15vh] w-[10vw] position: absolute ml-45"><p className="text-black mt-3 ml-2 flex">Figma: <br />IG: <br />LI:</p></div>
+              <l1 className="text-black text-8xl">TFOL</l1> 
+            </ul>
+            <l1 className="text-black text-8xl">TFOLIO</l1>
+          </div>
+        </section>
       </main>
     </div>
   );
